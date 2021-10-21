c216dc4a063f450278218f76d49852e91bd40469
Syrine Lahouel <syrinelahouel@Syrines-MacBook-Pro.local>
2021-10-21 16:53:14 +0200

    es6

--- es6.js
+++ es6.js
@@ -13,7 +13,74 @@ console.log(fruits)
 //  `Raspberry`. Sans utiliser la méthode unshift, seulement 
 //  la notation es6
 
-let fruits = ['Apple','Orange','Banana']
+fruits = ['Apple','Orange','Banana']
+fruits = [
+    "Raspberry",
+     ...fruits
+]
+console.log(fruits)
 
 
 //3
+
+// Créer les constantes suivantes:
+// const arr1 = ['A', 'B', 'C']
+// const arr2 = ['X', 'Y', 'Z']
+// Concatener ces deux tableaux pour avoir le résultat suivant:
+// ['A', 'B', 'C', 'X', 'Y', 'Z']
+
+const arr1 = ['A', 'B', 'C']
+const arr2 = ['X', 'Y', 'Z']
+const array = [
+    ...arr1,
+    ...arr2
+]
+
+console.log(array)
+
+//4
+// Créer une constante bobby qui sera un objet avec toutes les clés/valeurs 
+// de user, et en y ajoutant firstName: "bobby". Seulement avec la 
+// notation ES6
+
+const user = {
+    age: 10,
+    eyes: "vert",
+    legs: 2,
+    arms: 2,
+    hair: "long",
+    caracterstique: "gras du bide",
+    nose: "petit nez",
+    ears: "grandes oreilles",
+    glasses: true,
+    sosie: "sarkozy"
+  }
+
+const bobby = {
+    ...user,
+    firstName: "Bobby"
+}
+
+console.log(bobby)
+
+
+
+//5
+//   Avec la notation es6, console log chaque element du 
+//   tableau avec une boucle forEach
+const numbers = [1, 2, 3, 4, 5, 6]
+
+
+
+//6
+// Avec la notation es6, créer un nouveau tableau contenant seulement 
+// les nombres impairs
+// const numbers = [1, 2, 3, 4, 5, 6]
+
+
+
+//7
+// Avec la notation es6, créer un nouveau tableau contenant chaque 
+// element au carré
+
+// const numbers = [1, 2, 3, 4, 5, 6]
