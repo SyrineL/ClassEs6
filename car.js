f133c038a84f4bca6004d9bfc80a83f255897581
Syrine Lahouel <syrinelahouel@Syrines-MacBook-Pro.local>
2021-10-21 14:37:56 +0200

    first commit

---
+++ car.js
@@ -0,0 +1,20 @@
+// ## 1 - Batmobile
+
+// - Créer un fichier `car.js`
+// - Dans car.js, créer un class `Car` avec 2 propriétés de base:
+//     - `brand`
+//     - `speed`
+// - Ajouter 3 méthodes à la classe `Car`:
+//     - `accelerate` (accélérer) qui prendra en paramètre un nombre. 
+//     Cette méthode va ajouter le nombre a la propriété `speed` définie plus haut
+//     - `brake` (freiner) qui prendra en paramètre un nombre. Cette méthode va soustraire le nombre 
+//     à la propriété `speed` définie plus haut
+//     - `describe` qui va afficher  `xxx running at yyy km/h` avec `brand` a la place de 
+//     `xxx` et `speed` a la place de `yyy`
+// - Créer une variable `ford` et  lui assigner l'initialisation de la classe avec la 
+// brand `ford` et speed `0` et afficher la variable ford dans la console
+// - Exécuter la fonction `accelerate` de ford avec une vitesse de 50 km/h et afficher 
+// `ford` dans la console
+// - Exécuter la fonction `brake` de ford avec une vitesse de 25 km/h et afficher `ford` 
+// dans la console
+// - Faites la même chose avec une nouvelle voiture
